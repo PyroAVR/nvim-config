76776c27bcdaf120547912ae1065889d7d20d90f
Andy "PyroAVR" Meyer <ahm7197@rit.edu>
2024-06-28 00:02:46 -0400

    handle keymaps as well as options on filetype trigger

--- lua/ftcustom.lua
+++ lua/ftcustom.lua
@@ -1,39 +1,72 @@
-default = {
-    expandtab = true,
-    shiftwidth = 4,
-    tabstop = 4
+-- simple inheritable table
+ftconfig = {
+    __index = function(table, key) return table["_base"][key] end,
+    new = function(self, base)
+        t = { _base = base or {} }
+        setmetatable(t, ftconfig)
+        return t
+    end
 }
 
-short = {
-    expandtab = true,
-    shiftwidth = 2,
-    tabstop = 2
+default = ftconfig:new{
+    options = {
+        expandtab = true,
+        shiftwidth = 4,
+        tabstop = 4
+    }
 }
 
-makefile = {
-    expandtab = false,
-    shiftwidth = 4,
-    tabstop = 4
+short = ftconfig:new{
+    options = {
+        expandtab = true,
+        shiftwidth = 2,
+        tabstop = 2
+    }
 }
 
-chain = {}
-chain.__index = function(table, key) return table["_base"][key] end
-chain.__call = function(base)
-    t = { _base = base }
-    setmetatable(t, chain)
-    return t
-end
+makefile = ftconfig:new{
+    options = {
+        expandtab = false,
+        shiftwidth = 4,
+        tabstop = 4
+    }
+}
+
+
+
+cpp = ftconfig:new(default)
+cpp.keymaps = {
+    {
+        mode = "i",
+        pattern = "endl",
+        subst = "<< std::endl",
+    },
+    {
+        mode = "i",
+        pattern = "cout",
+        subst = "std::cout << ",
+    },
+}
+
+python = ftconfig:new(default)
+python.keymaps = {
+    {
+        mode = "i",
+        pattern = "dbg",
+        subst = "import pudb; pudb.set_trace()"
+    },
+}
 
 -- ftmap is a table of file type names (str) -> config tables (table)
 ftmap = {
     asm = default,
     c = default,
-    cpp = default,
+    cpp = cpp,
     html = short,
     lua = default,
     openscad = default,
     plaintex = default,
-    python = default,
+    python = python,
     sh = default,
     tex = default,
     vim = default,
@@ -42,13 +75,28 @@ ftmap = {
 }
 
 -- apply ftmap autocmds that trigger on FileType events
+-- nvim will then apply these settings when the FileType event is triggered,
+-- aka enter / leave a buffer of the specified type.
 for ft, settings in pairs(ftmap) do
-    vim.api.nvim_create_autocmd("FileType", {
-        pattern = ft,
-        callback = function(args)
-            for key,val in pairs(settings) do
-                vim.o[key] = val
+    -- apply editor options
+    if settings.options ~= nil then
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = ft,
+            callback = function(args)
+                for key,val in pairs(settings.options) do
+                    vim.o[key] = val
+                end
             end
-        end
-    })
+        })
+    end
+    if settings.keymaps ~= nil then
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = ft,
+            callback = function(args)
+                for _, mapping in pairs(settings.keymaps) do
+                    vim.keymap.set(mapping.mode, mapping.pattern, mapping.subst)
+                end
+            end
+        })
+    end
 end
